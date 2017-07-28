fc3479468ca6efc2ea4d7b5afa022035db6ef4cd
a-ray-of-sunshine <884105526@qq.com>
2017-07-28 21:11:18 +0800

    解决冲突

--- nebula-web/src/main/java/com/cxy/entity/IdEntity.java
+++ nebula-web/src/main/java/com/cxy/entity/IdEntity.java
@@ -1,7 +1,14 @@
 package com.cxy.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class IdEntity {
 
+    @Id
+    @GeneratedValue
 	private Long id;
 
 	public Long getId() {
