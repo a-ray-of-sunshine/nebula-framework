3367d2e7603a6f80fe1eeb55f3db065e7ddd3d07
a-ray-of-sunshine <884105526@qq.com>
2017-08-08 18:51:25 +0800

    调整配置

--- nebula-core/src/main/java/com/nebula/server/impl/Tomcat.java
+++ nebula-core/src/main/java/com/nebula/server/impl/Tomcat.java
@@ -2,42 +2,78 @@ package com.nebula.server.impl;
 
 import java.io.File;
 
-import org.apache.catalina.startup.CatalinaProperties;
+import javax.servlet.ServletException;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.tomcat.util.scan.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.nebula.server.IServer;
 
-public class Tomcat implements IServer{
+public class Tomcat implements IServer {
 
-    private static File configurationDir;
-    static {
-        configurationDir = new File("D:\\workspace\\nebula-framework\\nebula-web\\target\\tomcat");
-        System.setProperty("catalina.base", configurationDir.getAbsolutePath());
-        // Trigger loading of catalina.properties
-        CatalinaProperties.getProperty("foo");
+    private org.apache.catalina.startup.Tomcat tomcat;
+    private static final Logger log = LoggerFactory.getLogger(Tomcat.class);
+
+    Tomcat() {
+        tomcat = new org.apache.catalina.startup.Tomcat();
     }
 
     @Override
     public void init(int port, String contextPath) {
-        // TODO Auto-generated method stub
-        
+        setEnv();
+
+        try {
+            // 添加 docBase
+            File file = new File("src/main/webapp");
+            tomcat.addWebapp(contextPath, file.getAbsolutePath());
+            tomcat.getConnector().setPort(port);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setEnv() {
+        // 2. 禁止扫描 classpath 中的 jar 包，查找 tld 文件，加快启动速度
+        System.setProperty(Constants.SKIP_JARS_PROPERTY, "*.jar");
+
+        // 2. 创建 work 目录，用来存储 jsp 编译后的文件
+        File f = new File("target/tomcat/");
+        f.mkdirs();
+        System.setProperty("catalina.base", f.getAbsolutePath());
     }
 
     @Override
     public void start() {
-        // TODO Auto-generated method stub
-        
+        try {
+            long t1 = System.nanoTime();
+            tomcat.start();
+            long t2 = System.nanoTime();
+            log.info("Server startup in {} s (total: {} ms)", (t2 - t1) / 1000000000, (t2 - t1) / 1000000);
+            tomcat.getServer().await();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void reload() {
-        // TODO Auto-generated method stub
-        
+        try {
+            tomcat.stop();
+            tomcat.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void stop() {
-        // TODO Auto-generated method stub
-        
+        try {
+            tomcat.stop();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- nebula-web/src/main/java/com/cxy/Main.java
+++ nebula-web/src/main/java/com/cxy/Main.java
@@ -6,22 +6,22 @@ import java.util.Map;
 import com.nebula.server.IServer;
 import com.nebula.server.IServerFactory;
 import com.nebula.server.impl.JettyFactory;
+import com.nebula.server.impl.TomcatFactory;
 
 public class Main {
 
     public static void main(String[] args) {
+        
         Map<String, String> config = new HashMap<>();
         config.put(JettyFactory.PORT, "8080");
-        config.put(JettyFactory.CONTEXT, "/");
+        config.put(JettyFactory.CONTEXT, "");
 
-        IServerFactory serverFactory = new JettyFactory();
+        // IServerFactory serverFactory = new JettyFactory();
+        IServerFactory serverFactory = new TomcatFactory();
         IServer server = serverFactory.createServer(config);
         
         server.start();
-        try {
-            Thread.sleep(99999999);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
+
+  
 }
