5c17a58b471a9522609c23e445019daa3f9bf083
a-ray-of-sunshine <884105526@qq.com>
2017-08-09 22:56:17 +0800

    开发 XMLPropertyTokensParser
    用来解析 XML 文件的 ${...} 配置

---
+++ nebula-core/src/main/java/com/nebula/utils/XMLPropertyTokensParser.java
@@ -0,0 +1,112 @@
+package com.nebula.utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 用来解析 XML 文件的 ${...}
+ */
+public class XMLPropertyTokensParser {
+    
+    private static final Logger log = LoggerFactory.getLogger(XMLPropertyTokensParser.class);
+    private static final String TOKEN_TAG = "$";
+    private static final String START_TAG = "{";
+    private static final String END_TAG = "}";
+
+    private static Pattern pattern;
+
+    private Properties configurationProperties;
+    private ClassLoader resourceLoader;
+    
+    static{
+        String reg = "\\" + TOKEN_TAG +  "\\s*\\" + START_TAG + "([\\w\\.\\s*]*)\\" + END_TAG;
+        pattern = Pattern.compile(reg);
+    }
+    
+    public XMLPropertyTokensParser(String propertyName){
+        this(propertyName, null);
+    }
+
+    public XMLPropertyTokensParser(String propertyName, Properties exProperties){
+        initLoader();
+        
+        URL url = resourceLoader.getResource(propertyName);
+        if(null != url){
+            try {
+
+                InputStream stream = url.openStream();
+                configurationProperties = new Properties();
+                configurationProperties.load(stream);
+                stream.close();
+                
+                if(null != exProperties) {
+                    configurationProperties.putAll(exProperties);
+                }
+            } catch (IOException e) {
+                log.error("读取资源 {} 失败，请检查配置", propertyName);
+            }
+            
+        }
+    }
+    
+    public InputStream parseTokens(InputStream stream) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        StringWriter source = new StringWriter(1024);
+        BufferedWriter cache = new BufferedWriter(source, 512);
+        try {
+            String line ;
+            while(null != (line = br.readLine())){
+                if(line.contains("$")) {
+                   line = parseProperties(line); 
+                }
+                cache.write(line);
+                cache.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return new ByteArrayInputStream(source.getBuffer().toString().getBytes());
+    }
+    
+    private String parseProperties(String line) {
+        // TODO 解析一行文本中的  ${...}
+        // <property name="driverClass" value="${jdbc.driverClass}"/>
+        Matcher matcher = pattern.matcher(line);
+        while(matcher.find()){
+            String token = matcher.group();
+            line = line.replace(token, resolve(token));
+        }
+        return line;
+    }
+
+    private String resolve(String token) {
+        token = token.substring(token.indexOf(START_TAG) + 1, token.indexOf(END_TAG)).trim();
+        String property = configurationProperties.getProperty(token);
+        if(null == property){
+           log.warn("Token resovle fail. can't find token: {}" + token); 
+           property = token;
+        }
+        return property;
+    }
+
+    private void initLoader() {
+       resourceLoader = Thread.currentThread().getContextClassLoader();
+       if(null != resourceLoader) {
+          resourceLoader = ClassLoader.getSystemClassLoader(); 
+       }
+    }
+
+}
---
+++ nebula-core/src/test/java/com/nebula/server/TokenTest.java
@@ -0,0 +1,49 @@
+package com.nebula.server;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import com.nebula.utils.XMLPropertyTokensParser;
+
+public class TokenTest {
+    
+    private static final String TOKEN_TAG = "$";
+    private static final String START_TAG = "{";
+    private static final String END_TAG = "}";
+    
+    @Test
+    public void test1() throws IOException{
+        
+        XMLPropertyTokensParser parser = new XMLPropertyTokensParser("jdbc.properties");
+        URL url = Thread.currentThread().getContextClassLoader().getResource("hibernate.cfg.xml");
+        parser.parseTokens(url.openStream());
+    }
+
+    // @Test
+    public void test(){
+        // String reg = ".*?(\\$\\s*\\{([\\w\\.\\s*]*)\\}).*?";
+        String reg = "\\" + TOKEN_TAG +  "\\s*\\" + START_TAG + "([\\w\\.\\s*]*)\\" + END_TAG;
+        Pattern pattern = Pattern.compile(reg);
+        String test = "<property name=\"$ { jdbc.driverClassName }\" value=\"${jdbc.driverClass}\"/>";
+
+        Matcher matcher = pattern.matcher(test);
+        while(matcher.find()){
+            String token = matcher.group();
+            String normal = resolve(token);
+            test = test.replace(token, normal);
+        }
+        
+        
+        System.out.println(test);
+        
+        }
+
+    private String resolve(String token) {
+        token = token.substring(token.indexOf(START_TAG) + 1, token.indexOf(END_TAG));
+        return token.trim();
+    }
+}
